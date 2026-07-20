606a050d7e38f955921a8563ac6bc35f04eae9e0
antony <antonyfernandes644@gmail.com>
2026-07-20 22:16:32 +0530

    Comment Added

--- lib/modules/liturgical_engine/application/liturgical_engine.dart
+++ lib/modules/liturgical_engine/application/liturgical_engine.dart
@@ -1,6 +1,7 @@
 import '../domain/entities/liturgical_day.dart';
 import '../domain/entities/liturgical_year.dart';
 
+/// Interface for the LiturgicalEngine, defining methods to retrieve liturgical days and years.
 abstract interface class LiturgicalEngine {
   LiturgicalDay getDay(DateTime date);
   LiturgicalYear getYear(int year);
